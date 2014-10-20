8b5dfa4b9d2d2f429bef2ce093f9232526457f02
Toño Alvarez <elangeldelasalmas@hotmail.com>
2014-10-20 00:21:53 -0500

    Easy Node Authentication: Facebook
    Parte 2 de 5
    
    http://goo.gl/TNmXCw

---
+++ config/auth.js
@@ -0,0 +1,25 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '652498118198606', // your App ID
+		'clientSecret' 	: '76c1538d675005e2ae1c76fcc40b409f', // your App Secret
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	}
+
+};
+
